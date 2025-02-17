7b759feca5b9854ca2ca398b3e27a1836caab8ae
Tech Gammer <umairmughalflh@gmail.com>
2025-02-17 23:34:41 +0500

    All done Project

--- lib/main.dart
+++ lib/main.dart
@@ -49,7 +49,7 @@ class MyApp extends StatelessWidget {
       builder: (context, languageProvider, child) {
         return MaterialApp(
           debugShowCheckedModeBanner: false,
-          home:  Dashboard(),
+          home:  LoginPage(),
           theme: ThemeData(
             fontFamily: languageProvider.isEnglish ? 'Roboto' : 'JameelNoori',
           ),
